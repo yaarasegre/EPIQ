91a15de037aed4d671a19d1773b12be8c8fb1d6c
yaarasegre <yaarasegre@gmail.com>
2014-04-24 20:56:15 +0300

    Added sample files

---
+++ sample/runEPIQ.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../AlgorithmRelease/projections -p pheno.txt -f snps.epi -F ../parms/n1000_m100000/thresh_L10000_pow0.80.csv -e ../parms/edges.csv -n 1000 -L 10000 -C 46.3285 -M sample.map -D 0
